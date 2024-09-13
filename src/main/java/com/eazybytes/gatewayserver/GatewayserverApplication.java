6abbb02c9be66b1d7060d3d0860a1e574dff6be0
theabdul97@gmail.com <theabdul97@gmail.com>
2024-09-13 11:00:03 +0530

    RateLimiterChanges

--- src/main/java/com/eazybytes/gatewayserver/GatewayserverApplication.java
+++ src/main/java/com/eazybytes/gatewayserver/GatewayserverApplication.java
@@ -4,9 +4,13 @@ import java.time.LocalDateTime;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class GatewayserverApplication {
@@ -20,20 +24,34 @@ public class GatewayserverApplication {
 		return routeLocatorBuilder.routes()
 				.route(p -> p.path("/eazybank/accounts/**")
 						.filters(f -> f.rewritePath("/eazybank/accounts/(?<segment>.*)", "/${segment}")
-								.addResponseHeader("X-response-Time", LocalDateTime.now().toString()) //adding filter
-						.circuitBreaker(config->config.setName("accountsCircuitBreaker") //CiscuitBreaker name
-						.setFallbackUri("forward:/contact-support")))
+								.addResponseHeader("X-response-Time", LocalDateTime.now().toString()) // adding filter
+								.circuitBreaker(config -> config.setName("accountsCircuitBreaker") // CiscuitBreaker
+																									// name
+										.setFallbackUri("forward:/contact-support")))
 						.uri("lb://ACCOUNTS"))
 				.route(p -> p.path("/eazybank/accounts/**")
 						.filters(f -> f.rewritePath("/eazybank/accounts/(?<segment>.*)", "/${segment}")
-								.addResponseHeader("X-response-Time", LocalDateTime.now().toString())) //adding filter
+								.addResponseHeader("X-response-Time", LocalDateTime.now().toString())) // adding filter
 						.uri("lb://LOANS"))
 				.route(p -> p.path("/eazybank/accounts/**")
 						.filters(f -> f.rewritePath("/eazybank/accounts/(?<segment>.*)", "/${segment}")
-								.addResponseHeader("X-response-Time", LocalDateTime.now().toString())) //adding filter
+								.addResponseHeader("X-response-Time", LocalDateTime.now().toString()) // adding filter
+								.requestRateLimiter(config->config.setRateLimiter(redisRateLimiter()) //RateLimiter changes and inclusinf below 2beans
+										.setKeyResolver(userkeyResolver())))
 						.uri("lb://CARDS"))
 				.build();
 
+	}
+
+	@Bean
+	public RedisRateLimiter redisRateLimiter() {
+		return new RedisRateLimiter(1, 1, 1);
+	}
+
+	@Bean
+	KeyResolver userkeyResolver() {
+		return exchange -> Mono.justOrEmpty(exchange.getRequest().getHeaders().getFirst("user"))
+				.defaultIfEmpty("anonymouse");
 	}
 
 }
